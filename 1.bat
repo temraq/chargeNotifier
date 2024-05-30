75e7b8e7ce3776e323e64c8ae72750cc9b5f5bbf
temraq <40861165+temraq@users.noreply.github.com>
2024-05-30 08:39:56 +0300

    1.bat

--- 1.bat
+++ 1.bat
@@ -1,29 +1,30 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Запуск скрипта в новом окне, если не запущен в фоновом режиме
-
-rem Далее какой-нибудь полезный код
 
 :loop
-:: Получение информации о батарее
+
 for /f "tokens=2 delims==" %%i in ('wmic path Win32_Battery get EstimatedChargeRemaining /format:value') do (
-    set charge=%%i
+    if not "%%i"=="" set charge=%%i
 )
 
-:: Удаление пробелов в начале и конце строки
-set charge=%charge: =%
+for /f "tokens=2 delims==" %%i in ('wmic path Win32_Battery get BatteryStatus /format:value') do (
+    if not "%%i"=="" set status=%%i
+)
 
-:: Проверка заряда батареи и воспроизведение аудио
-if "%charge%"=="100" (
-    :: Воспроизведение системного звука
+
+set charge=%charge: =%
+set status=%status: =%
+
+
+if "%charge%"=="100" if "%status%"=="2" (
+
     powershell -c "$PlayWav=New-Object System.Media.SoundPlayer; $PlayWav.SoundLocation='1.wav'; $PlayWav.playsync()"
 ) 
 
-:: Ожидание 60 секунд перед следующей проверкой
 timeout /t 60 /nobreak > nul
 
-:: Переход в начало цикла
+
 goto loop
 
 endlocal
